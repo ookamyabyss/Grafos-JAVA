147526f9a28a0c0f5c8a6cff9d2cc68076ed45ed
OokamyAbyss474 <102814036+ookamyabyss@users.noreply.github.com>
2022-07-02 14:03:04 -0300

    att main// op de add ver or ars

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
-
 // Uneb. Universidade do Estado da Bahia
 // Aluno. Rafael Roberto Coutinho da Cruz
+
+import java.text.BreakIterator;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Grafo g = new Grafo();
+      
         Vertice v1 = new Vertice(1);
         Vertice v2 = new Vertice(2);
         Vertice v3 = new Vertice(3);
@@ -15,50 +18,92 @@ public class Main {
         Aresta a4 = new Aresta(v1, v4);
         Aresta a3 = new Aresta(v2, v3);
         
-        // ADICIONAR VERTICES E ARESTAS ENTRE DOIS VERTICES
+        // ADICIONAR VERTICES 
         g.addVertice(v1);
         g.addVertice(v2);
         g.addVertice(v3);
         g.addVertice(v4);
-
+        // ADICIONAR ARESTAS ENTRE DOIS VERTICES
         g.addAresta(a1);
         g.addAresta(a2);
         g.addAresta(a3);
         g.addAresta(a4);
-                
-        // MOSTRAR VERTICES E ARESTAS NO GRAFO
-        //g.printVertices();
-        //g.printConections();
-        
-        // Matriz Adjacencia
-        //g.matrizAdjacencia();
-
-        // DELETAR VERTICE
-        // System.out.println("deletando vertice:");
-        //g.deleteVertice(1);
-        // g.printVertices();
-        // g.printConections();
-        
       
-        // DELETAR ARESTA
-        // System.out.println("deletando aresta:");
-        // g.deleteAresta(1, 2);
-        // g.printVertices();
-        // g.printConections();
-        
+        Scanner input = new Scanner(System.in);
+        boolean Menu = true;
+        int valor = 0;
+      
+        while (Menu) {
+          System.out.println("0 - Sair do programa");
+          System.out.println("1 - Vertices");
+          System.out.println("2 - Arestas");
+          System.out.println("3 - Matriz Adjacencia");
+          System.out.println("4 - Grau Menor, Medio, Maior");
+          System.out.println("5 - Grau Conexo ");
+          System.out.println("6 - Caminho Euler ");
+          System.out.println("7 - Grau do Vertice ");
+          System.out.println("8 - Adjacencias Vertice ");
+          System.out.println("9 - Deleta Vertice ");
+          System.out.println("10 - Deleta Aresta ");
 
-        // GRAU DO VERTICE
-        // System.out.println(g.grauVertice(1));
+          System.out.print("Escolha uma opção : ");
+          int option = input.nextInt();
+          System.out.println(); 
+          
+          switch (option) {
+            
+            case 1:
+              g.printVertices(); // MOSTRAR VERTICES  NO GRAFO         
+            break;
+            case 2:
+              g.printConections(); // MOSTRAR ARESTAS NO GRAFO           
+            break;
+            case 3:
+              g.matrizAdjacencia(); // MATRIZ ADJACENCIAS         
+            break;
+            case 4:
+              g.grau(); // GRAU MINIMO, MEDIO, MAXIMO        
+            break;
+            case 5:
+              boolean conexo = g.checkConexo(false); // CHECAR GRAFO CONEXO
+              if(conexo)
+                System.out.print("GRAFO CONEXO"); 
+              else 
+                System.out.print("GRAFO NÃO CONEXO");
+              System.out.println("\n");
+            break;
+            case 6:
+              g.caminhodeEuler(); /// CAMINHO DE EULER
+            break;
+            case 7: 
+              System.out.print("DIGITE O VERTICE PARA VÊ O GRAU ");
+              valor = input.nextInt();
+              System.out.println("GRAU DO VERTICE: "+g.grauVertice(valor)); // GRAU DO VERTICE
+              System.out.println("\n");
+            break;
+            case 8:
+              System.out.print("DIGITE O VERTICE PARA VÊ AS ADJACENCIAS ");
+              valor = input.nextInt();
+              g.adjacencias(valor);
+            break;
+            case 9:
+              System.out.println("Deletando Vertice:");
+              valor = input.nextInt();
+              g.deleteVertice(valor); // DELETAR VERTICE
+            break;
+            case 10:
+            System.out.println("Deletando Aresta:");
+            System.out.print("DIGITE O VERTICE INICIAL: ");
+            int vInicio = input.nextInt();
+            System.out.print("DIGITE O VERTICE FINAL: ");
+            int vFinal = input.nextInt();
+            g.deleteAresta(vInicio, vFinal); // DELETAR ARESTA
+            break;
+            default:
+              Menu = false;
+          }
+      }
 
-        //GRAU MINIMO, MEDIO, MAXIMO
-        //g.grau();
-
-        // CHECAR GRAFO CONEXO
-        // g.checkConexo();
-
-        // ADJACENCIAS DE UM VERTICE
-        // g.adjacencias(1);
-
-        
+ 
     }
 }
